0f42f35f323c297d4f42278c0fba89acab92b9ad
oceanbupt <tang358612022>
2020-03-30 18:58:35 +0800

    update

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ import pandas as pd
 from GBDT.gbdt import GradientBoostingRegressor
 from GBDT.gbdt import GradientBoostingBinaryClassifier
 from GBDT.gbdt import GradientBoostingMultiClassifier
-
+# author
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
